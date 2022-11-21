f73abc6962d8e1057205bb35067136f6a4d48e17
tuyen <20130459@st.hcmuaf.edu.vn>
2022-11-21 23:42:28 +0700

    add _LayoutPage for admin

---
+++ Hoa-Chat-Thi-Nghiem-ASP-NET-MVC/Hoa-Chat-Thi-Nghiem-ASP-NET-MVC/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Hoa_Chat_Thi_Nghiem_ASP_NET_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
