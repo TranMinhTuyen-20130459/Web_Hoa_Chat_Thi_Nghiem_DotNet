b6d54d25df142dfb97b2304497bbe80e9a83dddb
NguyenPhuTai-20130390 <115602622+NguyenPhuTai-20130390@users.noreply.github.com>
2022-12-06 08:41:57 +0700

    them class loginCustomer trong AccountController

---
+++ Hoa-Chat-Thi-Nghiem-ASP-NET-MVC/Hoa-Chat-Thi-Nghiem-ASP-NET-MVC/models/LoginCustomerModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Hoa_Chat_Thi_Nghiem_ASP_NET_MVC.Models
+{
+    public class LoginCustomerModel
+    {
+        private string username;
+
+        private string password;
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
--- Hoa-Chat-Thi-Nghiem-ASP-NET-MVC/Model/service/CustomerService.cs
+++ Hoa-Chat-Thi-Nghiem-ASP-NET-MVC/Model/service/CustomerService.cs
@@ -1,19 +1,27 @@
-﻿using Model.entity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Model.dao;
+using Model.model;
+using System.Collections;
 
 namespace Model.service
 {
-    class CustomerService
+    public class CustomerService
     {
         // đăng nhập cho khách hàng
-        public static Customer checkLogin(String username,String password)
+        public static Customer checkLogin(string username, string password)
         {
-            
+            CustomerDAO dao = new CustomerDAO();
+            ArrayList listCustomer = dao.getCustomers(username);
+            if (listCustomer.Count > 0)
+            {
+            }
+            else
+            {
+                Customer customer = (Customer)listCustomer[0];
+                if (customer.Password.Equals(password)) return customer;
+            }
             return null;
-        } 
+        }
     }
 }
+
+
