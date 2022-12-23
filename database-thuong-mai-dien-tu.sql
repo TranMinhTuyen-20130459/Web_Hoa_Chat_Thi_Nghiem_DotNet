b8a766fb0755395b247a5e41240c3d6595e6dec7
Trần Minh Tuyên <114179423+TranMinhTuyen-20130459@users.noreply.github.com>
2022-12-23 08:19:27 +0700

    update database

--- database-thuong-mai-dien-tu.sql
+++ database-thuong-mai-dien-tu.sql
@@ -1,3 +1,26 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 100424
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : hoa_chat_thi_nghiem
+
+ Target Server Type    : MySQL
+ Target Server Version : 100424
+ File Encoding         : 65001
+
+ Date: 23/12/2022 08:11:07
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for account_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `account_admin`;
 CREATE TABLE `account_admin`  (
   `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `id_role_admin` int NOT NULL,
@@ -5,11 +28,18 @@ CREATE TABLE `account_admin`  (
   `passwordAD` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `time_created` timestamp NOT NULL DEFAULT current_timestamp,
   `time_change_pass` timestamp NOT NULL DEFAULT current_timestamp,
+  `fullname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`username`) USING BTREE,
   INDEX `id_status_acc`(`id_status_acc` ASC) USING BTREE,
-  INDEX `id_role_admin`(`id_role_admin` ASC) USING BTREE
+  INDEX `id_role_admin`(`id_role_admin` ASC) USING BTREE,
+  CONSTRAINT `account_admin_ibfk_1` FOREIGN KEY (`id_status_acc`) REFERENCES `status_acc` (`id_status_acc`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `account_admin_ibfk_2` FOREIGN KEY (`id_role_admin`) REFERENCES `role_admin` (`id_role_admin`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for account_customer
+-- ----------------------------
+DROP TABLE IF EXISTS `account_customer`;
 CREATE TABLE `account_customer`  (
   `id_user_customer` int NOT NULL AUTO_INCREMENT,
   `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
@@ -24,11 +54,17 @@ CREATE TABLE `account_customer`  (
   `time_created` timestamp NOT NULL DEFAULT current_timestamp,
   `time_change_pass` timestamp NOT NULL DEFAULT current_timestamp,
   PRIMARY KEY (`id_user_customer`) USING BTREE,
+  UNIQUE INDEX `username`(`username` ASC) USING BTREE,
   INDEX `id_status_acc`(`id_status_acc` ASC) USING BTREE,
   INDEX `id_city`(`id_city` ASC) USING BTREE,
-  UNIQUE INDEX `username`(`username` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  CONSTRAINT `account_customer_ibfk_1` FOREIGN KEY (`id_status_acc`) REFERENCES `status_acc` (`id_status_acc`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `account_customer_ibfk_2` FOREIGN KEY (`id_city`) REFERENCES `city` (`id_city`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for bill_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `bill_detail`;
 CREATE TABLE `bill_detail`  (
   `id_bill` int NOT NULL,
   `id_product` int NOT NULL,
@@ -36,9 +72,15 @@ CREATE TABLE `bill_detail`  (
   `listed_price` bigint NOT NULL,
   `current_price` bigint NOT NULL,
   PRIMARY KEY (`id_bill`, `id_product`) USING BTREE,
-  INDEX `id_product`(`id_product` ASC) USING BTREE
+  INDEX `id_product`(`id_product` ASC) USING BTREE,
+  CONSTRAINT `bill_detail_ibfk_1` FOREIGN KEY (`id_bill`) REFERENCES `bills` (`id_bill`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `bill_detail_ibfk_2` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for bills
+-- ----------------------------
+DROP TABLE IF EXISTS `bills`;
 CREATE TABLE `bills`  (
   `id_bill` int NOT NULL AUTO_INCREMENT,
   `id_user` int NOT NULL,
@@ -54,9 +96,16 @@ CREATE TABLE `bills`  (
   PRIMARY KEY (`id_bill`) USING BTREE,
   INDEX `id_user`(`id_user` ASC) USING BTREE,
   INDEX `id_status_bill`(`id_status_bill` ASC) USING BTREE,
-  INDEX `id_city`(`id_city` ASC) USING BTREE
+  INDEX `id_city`(`id_city` ASC) USING BTREE,
+  CONSTRAINT `bills_ibfk_2` FOREIGN KEY (`id_status_bill`) REFERENCES `status_bill` (`id_status_bill`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `bills_ibfk_3` FOREIGN KEY (`id_city`) REFERENCES `city` (`id_city`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `bills_ibfk_4` FOREIGN KEY (`id_user`) REFERENCES `account_customer` (`id_user_customer`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for city
+-- ----------------------------
+DROP TABLE IF EXISTS `city`;
 CREATE TABLE `city`  (
   `id_city` int NOT NULL AUTO_INCREMENT,
   `name_city` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -64,6 +113,10 @@ CREATE TABLE `city`  (
   PRIMARY KEY (`id_city`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for comment_news
+-- ----------------------------
+DROP TABLE IF EXISTS `comment_news`;
 CREATE TABLE `comment_news`  (
   `id_comment` int NOT NULL AUTO_INCREMENT,
   `id_news` int NOT NULL,
@@ -72,9 +125,15 @@ CREATE TABLE `comment_news`  (
   `content_comment` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id_comment`) USING BTREE,
   INDEX `id_news`(`id_news` ASC) USING BTREE,
-  INDEX `id_user_customer`(`id_user_customer` ASC) USING BTREE
+  INDEX `id_user_customer`(`id_user_customer` ASC) USING BTREE,
+  CONSTRAINT `comment_news_ibfk_1` FOREIGN KEY (`id_news`) REFERENCES `news` (`id_news`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `comment_news_ibfk_2` FOREIGN KEY (`id_user_customer`) REFERENCES `account_customer` (`id_user_customer`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for contact
+-- ----------------------------
+DROP TABLE IF EXISTS `contact`;
 CREATE TABLE `contact`  (
   `id_contact` int NOT NULL AUTO_INCREMENT,
   `full_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -86,6 +145,10 @@ CREATE TABLE `contact`  (
   PRIMARY KEY (`id_contact`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for infor_web
+-- ----------------------------
+DROP TABLE IF EXISTS `infor_web`;
 CREATE TABLE `infor_web`  (
   `id_infor` int NOT NULL AUTO_INCREMENT,
   `phone_web` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -94,24 +157,39 @@ CREATE TABLE `infor_web`  (
   PRIMARY KEY (`id_infor`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for news
+-- ----------------------------
+DROP TABLE IF EXISTS `news`;
 CREATE TABLE `news`  (
   `id_news` int NOT NULL AUTO_INCREMENT,
   `title_news` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `content_news` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `content_news` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `url_img_news` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
   `date_posted` timestamp NOT NULL DEFAULT current_timestamp ON UPDATE CURRENT_TIMESTAMP,
   `quantity_comment` int NOT NULL,
+  `nameAdmin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id_news`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for price_product
+-- ----------------------------
+DROP TABLE IF EXISTS `price_product`;
 CREATE TABLE `price_product`  (
   `id_product` int NOT NULL,
-  `date` date NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp,
   `listed_price` bigint NOT NULL,
   `current_price` bigint NOT NULL,
-  PRIMARY KEY (`id_product`, `date`) USING BTREE
+  `nameAdmin` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_product`, `date`) USING BTREE,
+  CONSTRAINT `price_product_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for products
+-- ----------------------------
+DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products`  (
   `id_product` int NOT NULL AUTO_INCREMENT,
   `id_type_product` int NOT NULL,
@@ -121,72 +199,88 @@ CREATE TABLE `products`  (
   `description_product` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
   `url_img_product` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
   `quantity_product` int NOT NULL,
-  `country` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `date_inserted` timestamp NOT NULL DEFAULT current_timestamp,
-  `star_review` tinyint NULL DEFAULT NULL,
+  `star_review` tinyint NULL DEFAULT 4,
+  `nameAdmin` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id_product`) USING BTREE,
   INDEX `id_type_product`(`id_type_product` ASC) USING BTREE,
   INDEX `id_status_product`(`id_status_product` ASC) USING BTREE,
-  INDEX `id_supplier`(`id_supplier` ASC) USING BTREE
+  INDEX `id_supplier`(`id_supplier` ASC) USING BTREE,
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id_type_product`) REFERENCES `type_product` (`id_type_product`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`id_status_product`) REFERENCES `status_product` (`id_status_product`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `products_ibfk_3` FOREIGN KEY (`id_supplier`) REFERENCES `suppliers` (`id_supplier`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for role_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `role_admin`;
 CREATE TABLE `role_admin`  (
   `id_role_admin` int NOT NULL,
   `name_role` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id_role_admin`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for sold_product
+-- ----------------------------
+DROP TABLE IF EXISTS `sold_product`;
 CREATE TABLE `sold_product`  (
   `id_product` int NOT NULL,
   `datetime` date NOT NULL DEFAULT current_timestamp,
   `quantity_sold` int NOT NULL,
-  PRIMARY KEY (`id_product`, `datetime`) USING BTREE
+  PRIMARY KEY (`id_product`, `datetime`) USING BTREE,
+  CONSTRAINT `sold_product_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for status_acc
+-- ----------------------------
+DROP TABLE IF EXISTS `status_acc`;
 CREATE TABLE `status_acc`  (
   `id_status_acc` int NOT NULL,
   `name_status_acc` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id_status_acc`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for status_bill
+-- ----------------------------
+DROP TABLE IF EXISTS `status_bill`;
 CREATE TABLE `status_bill`  (
   `id_status_bill` int NOT NULL,
   `name_status_bill` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id_status_bill`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for status_product
+-- ----------------------------
+DROP TABLE IF EXISTS `status_product`;
 CREATE TABLE `status_product`  (
   `id_status_product` int NOT NULL,
   `name_status_product` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id_status_product`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for suppliers
+-- ----------------------------
+DROP TABLE IF EXISTS `suppliers`;
 CREATE TABLE `suppliers`  (
   `id_supplier` int NOT NULL AUTO_INCREMENT,
   `name_supplier` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id_supplier`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Table structure for type_product
+-- ----------------------------
+DROP TABLE IF EXISTS `type_product`;
 CREATE TABLE `type_product`  (
   `id_type_product` int NOT NULL AUTO_INCREMENT,
   `name_type_product` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id_type_product`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
-ALTER TABLE `account_admin` ADD CONSTRAINT `account_admin_ibfk_1` FOREIGN KEY (`id_status_acc`) REFERENCES `status_acc` (`id_status_acc`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `account_admin` ADD CONSTRAINT `account_admin_ibfk_2` FOREIGN KEY (`id_role_admin`) REFERENCES `role_admin` (`id_role_admin`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `account_customer` ADD CONSTRAINT `account_customer_ibfk_1` FOREIGN KEY (`id_status_acc`) REFERENCES `status_acc` (`id_status_acc`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `account_customer` ADD CONSTRAINT `account_customer_ibfk_2` FOREIGN KEY (`id_city`) REFERENCES `city` (`id_city`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `bill_detail` ADD CONSTRAINT `bill_detail_ibfk_1` FOREIGN KEY (`id_bill`) REFERENCES `bills` (`id_bill`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `bill_detail` ADD CONSTRAINT `bill_detail_ibfk_2` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `bills` ADD CONSTRAINT `bills_ibfk_2` FOREIGN KEY (`id_status_bill`) REFERENCES `status_bill` (`id_status_bill`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `bills` ADD CONSTRAINT `bills_ibfk_3` FOREIGN KEY (`id_city`) REFERENCES `city` (`id_city`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `bills` ADD CONSTRAINT `bills_ibfk_4` FOREIGN KEY (`id_user`) REFERENCES `account_customer` (`id_user_customer`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `comment_news` ADD CONSTRAINT `comment_news_ibfk_1` FOREIGN KEY (`id_news`) REFERENCES `news` (`id_news`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `comment_news` ADD CONSTRAINT `comment_news_ibfk_2` FOREIGN KEY (`id_user_customer`) REFERENCES `account_customer` (`id_user_customer`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `price_product` ADD CONSTRAINT `price_product_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `products` ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id_type_product`) REFERENCES `type_product` (`id_type_product`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `products` ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`id_status_product`) REFERENCES `status_product` (`id_status_product`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `products` ADD CONSTRAINT `products_ibfk_3` FOREIGN KEY (`id_supplier`) REFERENCES `suppliers` (`id_supplier`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `sold_product` ADD CONSTRAINT `sold_product_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
+SET FOREIGN_KEY_CHECKS = 1;
