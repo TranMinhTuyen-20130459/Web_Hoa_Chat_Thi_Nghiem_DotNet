56010893f6d71b2737bf67f98dd1abb12a278cb4
VuCAP <20130465@st.hcmuaf.edu.vn>
2022-12-26 01:01:00 +0700

    Update Cart
    Thao tác thêm sản phẩm

---
+++ Hoa-Chat-Thi-Nghiem-ASP-NET-MVC/Hoa-Chat-Thi-Nghiem-ASP-NET-MVC/Assets/shop/js/update-cart-item.js
@@ -0,0 +1,21 @@
+﻿function addItems(idProduct, incremental) {
+    var quantity = getQuantity();
+    $.post("/Cart/AddItem?idProduct=" + idProduct + "&quantity=" + quantity + "&incremental=" + incremental);
+}
+
+function addItem(idProduct) {
+    $.post("/Cart/AddItem?idProduct=" + idProduct);
+}
+
+function minus(idProduct) {
+    $.post("/Cart/MinusQuantity?idProduct=" + idProduct);
+}
+
+function deleteItem(input) {
+    $.post("/Cart/RemoveItem?idProduct=" + input);
+}
+
+function getQuantity() {
+    var quantity = document.getElementsByTagName('input')[2].value;
+    console.log(quantity);
+}
